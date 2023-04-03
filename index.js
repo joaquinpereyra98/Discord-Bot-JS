fda271a4d6ee9b7f888a458bf07dae51bae0c064
joaquinpereyra98 <joaquinpereyra98@gmail.com>
2023-04-03 01:59:03 -0300

    Update index.js
    no more config.js, search other ways.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+// const { token } = require('./config.json');
+//config file is uselees, try better ways: .env or .gitnore
 
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
